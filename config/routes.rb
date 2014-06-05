605abec4bd1f0c8e435b5743547cb7733e3eaec5
dfudger <daniellefudger@gmail.com>
2014-06-05 14:44:07 -0400

    Fixed the users table and renamed the column type to group

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'users/edit'
-
-  get 'users/destroy'
-
-  get 'users/show'
-
-  get 'users/create'
-
-  get 'users/update'
-
-  get 'users/index'
-
-  get 'users/new'
-
   #/campaigns/1/videos/
   resources :admins
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602022742) do
+ActiveRecord::Schema.define(version: 20140605183918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 20140602022742) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
-    t.integer  "type"
+    t.integer  "group"
     t.string   "name"
     t.string   "city"
     t.string   "school"
