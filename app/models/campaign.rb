f0d0021171093a3e2efb118efd05e3cffedd782d
Robert Kuykendall <robert@rkuykendall.com>
2014-07-02 23:48:50 -0400

    Add display functions to model, and show them in templates.

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,6 +1,23 @@
 class Campaign < ActiveRecord::Base
   belongs_to :user
+  belongs_to :school
   has_many :donations
   has_many :videos
   has_many :images
+
+  def progress
+    ((self.pledged / self.goal)*100).to_i
+  end
+
+  def backers
+    self.donations.size
+  end
+
+  def pledged
+    self.donations.sum(:amount)
+  end
+
+  def left
+    (self.end.to_date - Date.today.to_date).to_i
+  end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,7 @@
 class Video < ActiveRecord::Base
   belongs_to :campaign
+
+  def embed
+    '//www.youtube.com/embed/'+self.url.split("v=")[1]
+  end
 end
